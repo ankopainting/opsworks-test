2e0ca0885cbf7fd47a8cbd828ccdfca9f3cb1612
Anko <anko.com+gh@gmail.com>
2013-05-28 08:42:01 +1000

    creating appropriate directories

--- nfsserver/recipes/default.rb
+++ nfsserver/recipes/default.rb
@@ -8,3 +8,31 @@
 #
 
 include_recipe "nfs::server"
+
+directory "/web" do
+	owner "root"
+	group "www-data"
+	mode 4664
+	action :create
+end
+
+directory "/web/sites/html/default" do
+	owner "root"
+	group "www-data"
+	mode 4664
+	action :create
+end
+
+directory "/web/sites/apps/default" do
+	owner "root"
+	group "www-data"
+	mode 4664
+	action :create
+end
+
+directory "/web/configs/managed" do
+	owner "root"
+	group "root"
+	mode 4644
+	action :create
+end
