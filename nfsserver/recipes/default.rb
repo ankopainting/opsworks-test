8ba7fc9349cf7c46aaffcb9df656f0f22be5bf58
Anko <anko.com+gh@gmail.com>
2013-05-28 08:58:41 +1000

    fixed directory permissions

--- nfsserver/recipes/default.rb
+++ nfsserver/recipes/default.rb
@@ -12,27 +12,37 @@ include_recipe "nfs::server"
 directory "/web" do
 	owner "root"
 	group "www-data"
-	mode 4664
+	mode 04664
 	action :create
 end
 
 directory "/web/sites/html/default" do
 	owner "root"
 	group "www-data"
-	mode 4664
+	mode 04664
+	recursive true
 	action :create
 end
 
 directory "/web/sites/apps/default" do
 	owner "root"
 	group "www-data"
-	mode 4664
+	mode 04664
+	recursive true
 	action :create
 end
 
 directory "/web/configs/managed" do
 	owner "root"
 	group "root"
-	mode 4644
+	mode 04644
+	recursive true
 	action :create
+end
+
+nfs_export "/web" do
+	network '172.31.28.0/24'
+	writeable false 
+	sync true
+	options ['no_root_squash']
 end
