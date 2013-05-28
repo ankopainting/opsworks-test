524875a91186b18f44bd9e7996afc0a4b73fd2d6
Anko <anko.com+gh@gmail.com>
2013-05-29 09:04:56 +1000

    added missing files

---
+++ web/attributes/default.rb
@@ -0,0 +1,4 @@
+
+# this is not used
+# default['nfs']['server'] = "172.31.18.212"
+
---
+++ web/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: web
+# Recipe:: default
+#
+#
+# sets up the web environment for a dete web server
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'apache2'
+
+package "libapache2-mod-macro" do
+	action :install
+	notifies :restart, resources(:service => 'apache2') 
+end
+
+# enable the macro module
+apache_module "macro"
+
+template "/var/www/index.html" do
+	mode 0664
+	source "index.html.erb"
+end
+
+# create directory to be mounted from centralised file server
+
+directory "/web" do
+	owner "root"
+	group "www-data"
+	mode 02775
+	recursive true
+	action :create
+end
+
+# mount directory
+
+node_handle = node[:content_repository_ip] # or node['nfs']['server'])
+node_handle += ":/web"
+
+mount "/web" do
+	device node_handle
+	fstype "nfs"
+	options "ro"
+	#actions [:mount, :enabled]
+end
